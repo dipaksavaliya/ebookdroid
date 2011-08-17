57e2a62b2d999abc9b9d12d12bb335e5bd02e073
alexander.v.kasatkin <alexander.v.kasatkin@714301b5-5bf2-3aa6-4ccc-ecebab0412e3>
2011-08-17 09:24:35 +0000

    EBookDroid version 1.1.1

---
+++ tags/1.1.1/src/org/ebookdroid/core/SinglePageDocumentView.java
@@ -0,0 +1,199 @@
+package org.ebookdroid.core;
+
+import org.ebookdroid.core.curl.PageAnimationType;
+import org.ebookdroid.core.curl.PageAnimator;
+import org.ebookdroid.core.utils.AndroidVersion;
+
+import android.graphics.Canvas;
+import android.graphics.RectF;
+import android.view.MotionEvent;
+import android.view.VelocityTracker;
+import android.view.View;
+
+/**
+ * The Class SinglePageDocumentView.
+ *
+ * Used in single page view mode
+ */
+public class SinglePageDocumentView extends AbstractDocumentView {
+
+    /** The curler. */
+    private PageAnimator curler;
+
+    /**
+     * Instantiates a new single page document view.
+     *
+     * @param baseActivity
+     *            the base activity
+     */
+    public SinglePageDocumentView(final BaseViewerActivity baseActivity) {
+        super(baseActivity);
+        updateAnimationType();
+    }
+
+    @Override
+    public void goToPageImpl(final int toPage) {
+        if (toPage >= 0 && toPage <= getBase().getDocumentModel().getPageCount()) {
+            final Page page = getBase().getDocumentModel().getPageObject(toPage);
+            getBase().getDocumentModel().setCurrentPageIndex(page.getDocumentPageIndex(), page.getIndex());
+            updatePageVisibility();
+        }
+        if (curler != null) {
+            curler.resetPageIndexes();
+        }
+    }
+
+    @Override
+    public int getCurrentPage() {
+        return getBase().getDocumentModel().getCurrentViewPageIndex();
+    }
+
+    @Override
+    protected void verticalConfigScroll(final int direction) {
+        goToPageImpl(getBase().getDocumentModel().getCurrentViewPageIndex() + direction);
+
+        invalidate();
+    }
+
+    @Override
+    protected void verticalDpadScroll(final int direction) {
+        goToPageImpl(getBase().getDocumentModel().getCurrentViewPageIndex() + direction);
+        invalidate();
+    }
+
+    @Override
+    protected int getTopLimit() {
+        final RectF bounds = getBase().getDocumentModel().getCurrentPageObject().getBounds();
+        return ((int) bounds.top > 0) ? 0 : (int) bounds.top;
+    }
+
+    @Override
+    protected int getLeftLimit() {
+        final RectF bounds = getBase().getDocumentModel().getCurrentPageObject().getBounds();
+        return ((int) bounds.left > 0) ? 0 : (int) bounds.left;
+    }
+
+    @Override
+    protected int getBottomLimit() {
+        final RectF bounds = getBase().getDocumentModel().getCurrentPageObject().getBounds();
+        return ((int) bounds.bottom < getHeight()) ? 0 : (int) bounds.bottom - getHeight();
+    }
+
+    @Override
+    protected int getRightLimit() {
+        final RectF bounds = getBase().getDocumentModel().getCurrentPageObject().getBounds();
+        return ((int) bounds.right < getWidth()) ? 0 : (int) bounds.right - getWidth();
+    }
+
+    @Override
+    public void scrollTo(final int x, final int y) {
+        super.scrollTo(Math.min(Math.max(x, getLeftLimit()), getRightLimit()),
+                Math.min(Math.max(y, getTopLimit()), getBottomLimit()));
+    }
+
+    @Override
+    public boolean onTouchEvent(final MotionEvent event) {
+        if (isCurlerDisabled()) {
+            return super.onTouchEvent(event);
+        } else {
+            if (getBase().getMultiTouchZoom() != null) {
+                if (getBase().getMultiTouchZoom().onTouchEvent(event)) {
+                    return true;
+                }
+                if (getBase().getMultiTouchZoom().isResetLastPointAfterZoom()) {
+                    setLastPosition(event);
+                    getBase().getMultiTouchZoom().setResetLastPointAfterZoom(false);
+                }
+            }
+
+            if (velocityTracker == null) {
+                velocityTracker = VelocityTracker.obtain();
+            }
+            velocityTracker.addMovement(event);
+
+            return curler.onTouchEvent(event);
+        }
+    }
+
+    private boolean isCurlerDisabled() {
+        if (curler == null) {
+            return true;
+        }
+        final PageAlign align = getAlign();
+        final float zoom = getBase().getZoomModel().getZoom();
+        return align != PageAlign.AUTO || zoom != 1.0f;
+    }
+
+    @Override
+    protected void onDraw(final Canvas canvas) {
+        if (isCurlerDisabled()) {
+            final Page page = getBase().getDocumentModel().getCurrentPageObject();
+            if (page != null) {
+                page.draw(canvas);
+            }
+        } else {
+            curler.onDraw(canvas);
+        }
+    }
+
+    /**
+     * Invalidate page sizes.
+     */
+    @Override
+    public void invalidatePageSizes() {
+        if (!isInitialized()) {
+            return;
+        }
+        final int width = getWidth();
+        final int height = getHeight();
+        final float zoom = getBase().getZoomModel().getZoom();
+
+        for (int i = 0; i < getBase().getDocumentModel().getPages().size(); i++) {
+            final Page page = getBase().getDocumentModel().getPages().get(i);
+
+            PageAlign effectiveAlign = getAlign();
+            if (getAlign() == PageAlign.AUTO) {
+                final float pageHeight = page.getPageHeight(width, zoom);
+                if (pageHeight > height) {
+                    effectiveAlign = PageAlign.HEIGHT;
+                } else {
+                    effectiveAlign = PageAlign.WIDTH;
+                }
+            }
+
+            if (effectiveAlign == PageAlign.WIDTH) {
+                final float pageHeight = page.getPageHeight(width, zoom);
+                page.setBounds(new RectF(0, ((height - pageHeight) / 2), width * zoom, pageHeight
+                        + ((height - pageHeight) / 2)));
+            } else {
+                final float pageWidth = page.getPageWidth(height, zoom);
+                page.setBounds(new RectF((width - pageWidth) / 2, 0, pageWidth + (width - pageWidth) / 2, height * zoom));
+            }
+        }
+        if (curler != null) {
+            curler.setViewDrawn(false);
+        }
+
+    }
+
+    @Override
+    public boolean isPageVisible(final Page page) {
+        return page.getIndex() == getCurrentPage();
+    }
+
+    @Override
+    public void updateAnimationType() {
+        final PageAnimationType type = getBase().getBookSettings().getAnimationType();
+        curler = PageAnimationType.create(type, this);
+
+        if (!AndroidVersion.lessThan3x) {
+            final int layerType = type.isHardwareAccelSupported() ? View.LAYER_TYPE_HARDWARE : View.LAYER_TYPE_SOFTWARE;
+            this.setLayerType(layerType, null);
+        }
+
+        if (curler != null) {
+            curler.init();
+        }
+    }
+
+}
---
+++ tags/1.1.1/src/org/ebookdroid/core/curl/SinglePageSqueezer.java
@@ -0,0 +1,72 @@
+package org.ebookdroid.core.curl;
+
+import org.ebookdroid.core.Page;
+import org.ebookdroid.core.SinglePageDocumentView;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
+
+public class SinglePageSqueezer extends AbstractPageSlider {
+
+    public SinglePageSqueezer(final SinglePageDocumentView singlePageDocumentView) {
+        super(PageAnimationType.SQUEEZER, singlePageDocumentView);
+    }
+
+    /**
+     * Draw the foreground
+     *
+     * @param canvas
+     * @param rect
+     * @param paint
+     */
+    @Override
+    protected void drawForeground(final Canvas canvas) {
+        Page page = view.getBase().getDocumentModel().getPageObject(foreIndex);
+        if (page == null) {
+            page = view.getBase().getDocumentModel().getCurrentPageObject();
+        }
+        if (page != null) {
+            final Bitmap fore = getBitmap(canvas);
+            final Canvas tmp = new Canvas(fore);
+            page.draw(tmp, true);
+
+            final Rect src = new Rect(0, 0, view.getWidth(), view.getHeight());
+            final RectF dst = new RectF(0, 0, view.getWidth() - mA.x, view.getHeight());
+            final Paint paint = new Paint();
+            paint.setFilterBitmap(true);
+            paint.setAntiAlias(true);
+            paint.setDither(true);
+            canvas.drawBitmap(fore, src, dst, paint);
+        }
+    }
+
+    /**
+     * Draw the background image.
+     *
+     * @param canvas
+     * @param rect
+     * @param paint
+     */
+    @Override
+    protected void drawBackground(final Canvas canvas) {
+        final Page page = view.getBase().getDocumentModel().getPageObject(backIndex);
+        if (page != null) {
+            final Bitmap back = getBitmap(canvas);
+            final Canvas tmp = new Canvas(back);
+            page.draw(tmp, true);
+
+            final Rect src = new Rect(0, 0, view.getWidth(), view.getHeight());
+            final RectF dst = new RectF(view.getWidth() - mA.x, 0, view.getWidth(), view.getHeight());
+            final Paint paint = new Paint();
+            paint.setFilterBitmap(true);
+            paint.setAntiAlias(true);
+            paint.setDither(true);
+            canvas.drawBitmap(back, src, dst, paint);
+        }
+
+    }
+
+}
