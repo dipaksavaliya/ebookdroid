1b527b641d28fc8399bb641c32e6900e15eb006c
alexander.v.kasatkin <alexander.v.kasatkin@714301b5-5bf2-3aa6-4ccc-ecebab0412e3>
2011-09-12 11:47:20 +0000

    Backported from trunk: Fixed view draw after scrolling in Single page mode.

--- branches/1.2.x/src/org/ebookdroid/core/AbstractDocumentView.java
+++ branches/1.2.x/src/org/ebookdroid/core/AbstractDocumentView.java
@@ -112,7 +112,8 @@ public abstract class AbstractDocumentView extends SurfaceView implements ZoomLi
 
             @Override
             public void run() {
-                updatePageVisibility(newPage, direction, getBase().getZoomModel().getZoom());
+                final ViewState viewState = updatePageVisibility(newPage, direction, getBase().getZoomModel().getZoom());
+                redrawView(viewState);
             }
         });
     }
