47651959762f690d68e887ed931c5c57c432f316
alexander.v.kasatkin <alexander.v.kasatkin@714301b5-5bf2-3aa6-4ccc-ecebab0412e3>
2011-09-30 15:49:52 +0000

    PDF Archos fix

--- branches/1.2.x/src/org/ebookdroid/pdfdroid/codec/PdfPage.java
+++ branches/1.2.x/src/org/ebookdroid/pdfdroid/codec/PdfPage.java
@@ -1,6 +1,5 @@
 package org.ebookdroid.pdfdroid.codec;
 
-import org.ebookdroid.core.BaseViewerActivity;
 import org.ebookdroid.core.codec.CodecPage;
 
 import android.graphics.Bitmap;
@@ -18,27 +17,29 @@ public class PdfPage implements CodecPage {
 
     private long pageHandle;
     private final long docHandle;
+    private final RectF mediaBox;
 
     private PdfPage(final long pageHandle, final long docHandle) {
         this.pageHandle = pageHandle;
         this.docHandle = docHandle;
+        this.mediaBox = getMediaBox();
     }
 
     @Override
     public int getWidth() {
-        return (int) (getMediaBox().width() * BaseViewerActivity.DM.xdpi / 72);
+        return PdfContext.getWidthInPixels(mediaBox.width());
     }
 
     @Override
     public int getHeight() {
-        return (int) (getMediaBox().height() * BaseViewerActivity.DM.xdpi / 72);
+        return PdfContext.getHeightInPixels(mediaBox.height());
     }
 
     @Override
     public Bitmap renderBitmap(final int width, final int height, final RectF pageSliceBounds) {
         final Matrix matrix = new Matrix();
-        matrix.postTranslate(-getMediaBox().left, -getMediaBox().top);
-        matrix.postScale(width / getMediaBox().width(), -height / getMediaBox().height());
+        matrix.postTranslate(-mediaBox.left, -mediaBox.top);
+        matrix.postScale(width / mediaBox.width(), -height / mediaBox.height());
         matrix.postTranslate(0, height);
         matrix.postTranslate(-pageSliceBounds.left * width, -pageSliceBounds.top * height);
         matrix.postScale(1 / pageSliceBounds.width(), 1 / pageSliceBounds.height());
@@ -94,7 +95,7 @@ public class PdfPage implements CodecPage {
         final int width = viewbox.width();
         final int height = viewbox.height();
 
-        if (useNativeGraphics /*&& AndroidVersion.VERSION >= 8*/) {
+        if (useNativeGraphics /* && AndroidVersion.VERSION >= 8 */) {
             Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
             if (renderPageBitmap(docHandle, pageHandle, mRect, matrixArray, bmp)) {
                 return bmp;
