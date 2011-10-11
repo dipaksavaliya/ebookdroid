3949c5c1c322b4df038960f419bbc0687c424cb3
alexander.v.kasatkin <alexander.v.kasatkin@714301b5-5bf2-3aa6-4ccc-ecebab0412e3>
2011-10-11 09:29:19 +0000

    Branch 1.2.x closed and removed.

--- branches/1.2.x/jni/Application.mk
+++
@@ -1,5 +0,0 @@
-#APP_ABI := armeabi armeabi-v7a
-
-APP_CFLAGS := -DHAVE_CONFIG_H -DTHREADMODEL=NOTHREADS -DDEBUGLVL=0 -Os
-
-APP_MODULES := jpeg libdjvu mupdf ebookdroid
--- branches/1.2.x/src/org/ebookdroid/core/settings/ISettingsChangeListener.java
+++
@@ -1,9 +0,0 @@
-package org.ebookdroid.core.settings;
-
-public interface ISettingsChangeListener {
-
-    void onAppSettingsChanged(AppSettings oldSettings, AppSettings newSettings, AppSettings.Diff diff);
-
-    void onBookSettingsChanged(BookSettings oldSettings, BookSettings newSettings, BookSettings.Diff diff, AppSettings.Diff appDiff);
-
-}
