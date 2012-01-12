46f0f1dc6f1210ed13318927443d60bfd0cbf427
alexander.v.kasatkin <alexander.v.kasatkin@714301b5-5bf2-3aa6-4ccc-ecebab0412e3>
2012-01-12 22:15:58 +0000

    Refactoring.

--- branches/mupdfupdate/src/org/ebookdroid/pdfdroid/codec/PdfContext.java
+++ branches/mupdfupdate/src/org/ebookdroid/pdfdroid/codec/PdfContext.java
@@ -1,13 +1,10 @@
 package org.ebookdroid.pdfdroid.codec;
 
-import org.ebookdroid.core.BaseViewerActivity;
 import org.ebookdroid.core.EBookDroidLibraryLoader;
 import org.ebookdroid.core.codec.AbstractCodecContext;
 import org.ebookdroid.core.codec.CodecDocument;
 
 import android.graphics.Bitmap;
-
-import java.lang.reflect.Field;
 
 public class PdfContext extends AbstractCodecContext {
 
@@ -16,8 +13,6 @@ public class PdfContext extends AbstractCodecContext {
     public static final Bitmap.Config BITMAP_CFG = Bitmap.Config.RGB_565;
 
     public static final Bitmap.Config NATIVE_BITMAP_CFG = Bitmap.Config.ARGB_8888;
-
-    private static Integer densityDPI;
 
     static {
         EBookDroidLibraryLoader.load();
@@ -32,37 +27,6 @@ public class PdfContext extends AbstractCodecContext {
     @Override
     public CodecDocument openDocument(final String fileName, final String password) {
         return new PdfDocument(this, fileName, password);
-    }
-
-    public static int getWidthInPixels(final float pdfWidth) {
-        return getSizeInPixels(pdfWidth, BaseViewerActivity.DM.xdpi);
-    }
-
-    public static int getHeightInPixels(final float pdfHeight) {
-        return getSizeInPixels(pdfHeight, BaseViewerActivity.DM.ydpi);
-    }
-
-    public static int getSizeInPixels(final float pdfHeight, float dpi) {
-        if (dpi == 0) {
-            // Archos fix
-            dpi = getDensityDPI();
-        }
-        if (dpi < 72) { // Density lover then 72 is to small
-            dpi = 72; // Set default density to 72
-        }
-        return (int) (pdfHeight * dpi / 72);
-    }
-
-    private static int getDensityDPI() {
-        if (densityDPI == null) {
-            try {
-                Field f = BaseViewerActivity.DM.getClass().getDeclaredField("densityDpi");
-                densityDPI = ((Integer) f.get(BaseViewerActivity.DM));
-            } catch (final Throwable ex) {
-                densityDPI = Integer.valueOf(120);
-            }
-        }
-        return densityDPI.intValue();
     }
 
     private static native boolean isNativeGraphicsAvailable();
--- branches/mupdfupdate/src/org/ebookdroid/xpsdroid/codec/XpsContext.java
+++ branches/mupdfupdate/src/org/ebookdroid/xpsdroid/codec/XpsContext.java
@@ -1,6 +1,5 @@
 package org.ebookdroid.xpsdroid.codec;
 
-import org.ebookdroid.core.BaseViewerActivity;
 import org.ebookdroid.core.EBookDroidLibraryLoader;
 import org.ebookdroid.core.codec.AbstractCodecContext;
 import org.ebookdroid.core.codec.CodecDocument;
@@ -8,9 +7,9 @@ import org.ebookdroid.core.codec.CodecDocument;
 import android.graphics.Bitmap;
 
 public class XpsContext extends  AbstractCodecContext {
-    
+
     public static final boolean useNativeGraphics;
-    
+
     public static final Bitmap.Config BITMAP_CFG = Bitmap.Config.RGB_565;
 
     public static final Bitmap.Config NATIVE_BITMAP_CFG = Bitmap.Config.ARGB_8888;
@@ -18,21 +17,6 @@ public class XpsContext extends  AbstractCodecContext {
     static {
         EBookDroidLibraryLoader.load();
         useNativeGraphics = isNativeGraphicsAvailable();
-    }
-    
-    public static int getWidthInPixels(final float pdfWidth) {
-        return getSizeInPixels(pdfWidth, BaseViewerActivity.DM.xdpi);
-    }
-
-    public static int getHeightInPixels(final float pdfHeight) {
-        return getSizeInPixels(pdfHeight, BaseViewerActivity.DM.ydpi);
-    }
-    
-    public static int getSizeInPixels(final float pdfHeight, float dpi) {
-        if (dpi < 72) { //Density lover then 72 is to small
-            dpi = 72; // Set default density to 72
-        }
-        return (int) (pdfHeight * dpi / 72);
     }
 
     @Override
@@ -44,6 +28,6 @@ public class XpsContext extends  AbstractCodecContext {
     public CodecDocument openDocument(final String fileName, final String password) {
         return new XpsDocument(this, fileName);
     }
-    
+
     private static native boolean isNativeGraphicsAvailable();
 }
