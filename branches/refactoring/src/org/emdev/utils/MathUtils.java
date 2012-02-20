51cb2658c927c56d4141c9a3b5c823837f9318d5
alexander.v.kasatkin <alexander.v.kasatkin@714301b5-5bf2-3aa6-4ccc-ecebab0412e3>
2012-02-20 10:00:36 +0000

    Round functions added.

--- branches/refactoring/src/org/emdev/utils/MathUtils.java
+++ branches/refactoring/src/org/emdev/utils/MathUtils.java
@@ -2,6 +2,7 @@ package org.emdev.utils;
 
 import android.graphics.Rect;
 import android.graphics.RectF;
+import android.util.FloatMath;
 
 public class MathUtils {
 
@@ -13,52 +14,63 @@ public class MathUtils {
         return new Rect((int) rect.left, (int) rect.top, (int) rect.right, (int) rect.bottom);
     }
 
-    public static Rect rect(final float left, final float top, final float right, float bottom) {
+    public static Rect rect(final float left, final float top, final float right, final float bottom) {
         return new Rect((int) left, (int) top, (int) right, (int) bottom);
     }
 
-    public static RectF zoom(final RectF rect, float zoom) {
-        return new RectF(zoom *  rect.left, zoom * rect.top, zoom * rect.right, zoom * rect.bottom);
+    public static RectF zoom(final RectF rect, final float zoom) {
+        return new RectF(zoom * rect.left, zoom * rect.top, zoom * rect.right, zoom * rect.bottom);
     }
 
-    public static RectF zoom(final Rect rect, float zoom) {
-        return new RectF(zoom *  rect.left, zoom * rect.top, zoom * rect.right, zoom * rect.bottom);
+    public static RectF zoom(final Rect rect, final float zoom) {
+        return new RectF(zoom * rect.left, zoom * rect.top, zoom * rect.right, zoom * rect.bottom);
     }
 
-    public static Rect zoom(final float left, final float top, final float right, final float bottom, float zoom) {
-        return new Rect((int)(zoom * left), (int)(zoom * top), (int)(zoom * right), (int)(zoom * bottom));
+    public static Rect zoom(final float left, final float top, final float right, final float bottom, final float zoom) {
+        return new Rect((int) (zoom * left), (int) (zoom * top), (int) (zoom * right), (int) (zoom * bottom));
     }
 
-    public static int min(int... values) {
+    public static int min(final int... values) {
         int min = Integer.MAX_VALUE;
-        for(int v : values) {
+        for (final int v : values) {
             min = Math.min(v, min);
         }
         return min;
     }
 
-    public static int max(int... values) {
+    public static int max(final int... values) {
         int max = Integer.MIN_VALUE;
-        for(int v : values) {
+        for (final int v : values) {
             max = Math.max(v, max);
         }
         return max;
     }
 
-    public static float fmin(float... values) {
+    public static float fmin(final float... values) {
         float min = Float.MAX_VALUE;
-        for(float v : values) {
+        for (final float v : values) {
             min = Math.min(v, min);
         }
         return min;
     }
 
-    public static float fmax(float... values) {
+    public static float fmax(final float... values) {
         float max = Float.MIN_VALUE;
-        for(float v : values) {
+        for (final float v : values) {
             max = Math.max(v, max);
         }
         return max;
     }
-    
+
+    public static float round(final float value, final float share) {
+        return FloatMath.floor(value * share) / share;
+    }
+
+    public static RectF round(final RectF rect, final float share) {
+        rect.left = FloatMath.floor(rect.left * share) / share;
+        rect.top = FloatMath.floor(rect.top * share) / share;
+        rect.right = FloatMath.floor(rect.right * share) / share;
+        rect.bottom = FloatMath.floor(rect.bottom * share) / share;
+        return rect;
+    }
 }
