b20b8af9f6e93c74ead05bddc290f8ce8e1e70a1
alexander.v.kasatkin <alexander.v.kasatkin@714301b5-5bf2-3aa6-4ccc-ecebab0412e3>
2012-02-22 16:13:45 +0000

    MathUtils

--- branches/refactoring/src/org/emdev/utils/MathUtils.java
+++ branches/refactoring/src/org/emdev/utils/MathUtils.java
@@ -77,4 +77,21 @@ public class MathUtils {
         rect.bottom = FloatMath.floor(rect.bottom * share) / share;
         return rect;
     }
+    
+    public static RectF floor(final RectF rect) {
+        rect.left = FloatMath.floor(rect.left);
+        rect.top = FloatMath.floor(rect.top);
+        rect.right = FloatMath.floor(rect.right);
+        rect.bottom = FloatMath.floor(rect.bottom);
+        return rect;
+    }
+    
+    public static RectF ceil(final RectF rect) {
+        rect.left = FloatMath.ceil(rect.left);
+        rect.top = FloatMath.ceil(rect.top);
+        rect.right = FloatMath.ceil(rect.right);
+        rect.bottom = FloatMath.ceil(rect.bottom);
+        return rect;
+    }
+    
 }
