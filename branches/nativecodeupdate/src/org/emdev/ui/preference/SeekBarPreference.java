80464fade69aef633a72e1c7696d2b6de5fd571f
mc.creat <mc.creat@714301b5-5bf2-3aa6-4ccc-ecebab0412e3>
2012-04-03 13:01:48 +0000

    Added simple SeekBarPreference dialog. Currently used only for brightnes. Compatible with EditTextPreference.

---
+++ branches/nativecodeupdate/src/org/emdev/ui/preference/SeekBarPreference.java
@@ -0,0 +1,112 @@
+package org.emdev.ui.preference;
+
+import org.ebookdroid.R;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.preference.DialogPreference;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+
+public final class SeekBarPreference extends DialogPreference implements OnSeekBarChangeListener {
+
+    // Namespaces to read attributes
+    private static final String PREFERENCE_NS = "http://ebookdroid.org";
+    private static final String ANDROID_NS = "http://schemas.android.com/apk/res/android";
+
+    // Attribute names
+    private static final String ATTR_DEFAULT_VALUE = "defaultValue";
+    private static final String ATTR_MIN_VALUE = "minValue";
+    private static final String ATTR_MAX_VALUE = "maxValue";
+
+    // Default values for defaults
+    private static final int DEFAULT_CURRENT_VALUE = 50;
+    private static final int DEFAULT_MIN_VALUE = 0;
+    private static final int DEFAULT_MAX_VALUE = 100;
+
+    // Real defaults
+    private final int defaultValue;
+    private final int maxValue;
+    private final int minValue;
+
+    // Current value
+    private int currentValue;
+
+    // View elements
+    private SeekBar mSeekBar;
+    private TextView mValueText;
+
+    public SeekBarPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        // Read parameters from attributes
+        minValue = attrs.getAttributeIntValue(PREFERENCE_NS, ATTR_MIN_VALUE, DEFAULT_MIN_VALUE);
+        maxValue = attrs.getAttributeIntValue(PREFERENCE_NS, ATTR_MAX_VALUE, DEFAULT_MAX_VALUE);
+        defaultValue = attrs.getAttributeIntValue(ANDROID_NS, ATTR_DEFAULT_VALUE, DEFAULT_CURRENT_VALUE);
+    }
+
+    @Override
+    protected View onCreateDialogView() {
+        currentValue = Integer.parseInt(getPersistedString(Integer.toString(defaultValue)));
+
+        // Inflate layout
+        LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.pref_seek_dialog, null);
+
+        // Setup minimum and maximum text labels
+        ((TextView) view.findViewById(R.id.pref_seek_min_value)).setText(Integer.toString(minValue));
+        ((TextView) view.findViewById(R.id.pref_seek_max_value)).setText(Integer.toString(maxValue));
+
+        // Setup SeekBar
+        mSeekBar = (SeekBar) view.findViewById(R.id.pref_seek_bar);
+        mSeekBar.setMax(maxValue - minValue);
+        mSeekBar.setProgress(currentValue - minValue);
+        mSeekBar.setOnSeekBarChangeListener(this);
+
+        // Setup text label for current value
+        mValueText = (TextView) view.findViewById(R.id.pref_seek_current_value);
+        mValueText.setText(Integer.toString(currentValue));
+
+        return view;
+    }
+
+    @Override
+    protected void onDialogClosed(boolean positiveResult) {
+        super.onDialogClosed(positiveResult);
+        if (!positiveResult) {
+            return;
+        }
+        if (shouldPersist()) {
+            persistString(Integer.toString(currentValue));
+        }
+        notifyChanged();
+    }
+
+    @Override
+    public CharSequence getSummary() {
+        String summary = super.getSummary().toString();
+        int value = Integer.parseInt(getPersistedString(Integer.toString(defaultValue)));
+        return String.format(summary, value);
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seek, int value, boolean fromTouch) {
+        currentValue = value + minValue;
+        mValueText.setText(Integer.toString(currentValue));
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        // TODO Auto-generated method stub
+
+    }
+}
