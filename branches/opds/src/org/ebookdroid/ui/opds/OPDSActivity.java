a2b7df50ecb8c89db25cd1cbc183156e7ce45f91
Andrei.Komarovskikh@gmail.com <Andrei.Komarovskikh@gmail.com@714301b5-5bf2-3aa6-4ccc-ecebab0412e3>
2012-05-16 14:55:13 +0000

    Add new OPDS

--- branches/opds/src/org/ebookdroid/ui/opds/OPDSActivity.java
+++ branches/opds/src/org/ebookdroid/ui/opds/OPDSActivity.java
@@ -7,15 +7,23 @@ import org.ebookdroid.opds.Feed;
 import org.ebookdroid.opds.Link;
 import org.ebookdroid.ui.opds.adapters.OPDSAdapter;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ExpandableListView;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 
 import org.emdev.ui.AbstractActionActivity;
@@ -25,6 +33,10 @@ import org.emdev.ui.actions.ActionMethod;
 import org.emdev.ui.actions.IActionController;
 import org.emdev.ui.actions.params.Constant;
 import org.emdev.utils.LengthUtils;
+import org.emdev.utils.preferences.JSONSharedPreferences;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class OPDSActivity extends AbstractActionActivity implements ExpandableListView.OnGroupClickListener,
         ExpandableListView.OnChildClickListener, OPDSAdapter.FeedListener {
@@ -35,6 +47,10 @@ public class OPDSActivity extends AbstractActionActivity implements ExpandableLi
 
     private Menu menu;
 
+    private HashMap<String, String> feeds = new HashMap<String, String>();
+
+    private Button addFeed;
+
     public OPDSActivity() {
     }
 
@@ -43,20 +59,95 @@ public class OPDSActivity extends AbstractActionActivity implements ExpandableLi
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.opds);
+        addFeed = (Button) findViewById(R.id.opdsaddfeed);
+
+        addFeed.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                final View childView = LayoutInflater.from(OPDSActivity.this).inflate(R.layout.alias_url, null);
+
+                final AlertDialog.Builder alert = new AlertDialog.Builder(OPDSActivity.this);
+
+                alert.setTitle("Adding new catalog");
+                alert.setMessage("Enter alias and URL of the catalog");
+                alert.setView(childView);
+
+                final EditText aliasEdit = (EditText) childView.findViewById(R.id.editAlias);
+                final EditText urlEdit = (EditText) childView.findViewById(R.id.editURL);
+
+                alert.setPositiveButton("Add feed", new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        OPDSActivity.this.addFeed(aliasEdit.getText().toString(), urlEdit.getText().toString());
+
+                    }
+                });
+
+                alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                    }
+                });
+
+                alert.show();
+            }
+        });
+
         list = (ExpandableListView) findViewById(R.id.opdslist);
-
-        final Feed flibusta = new Feed("Flibusta", "http://flibusta.net/opds");
-        final Feed plough = new Feed("Plough", "http://www.plough.com/ploughCatalog_opds.xml");
-
-        adapter = new OPDSAdapter(this, flibusta, plough);
-        adapter.addListener(this);
-
-        list.setAdapter(adapter);
         list.setOnGroupClickListener(this);
         list.setOnChildClickListener(this);
 
         list.setGroupIndicator(null);
         list.setChildIndicator(null);
+
+        feeds.clear();
+
+        try {
+            JSONObject jsonObj = JSONSharedPreferences.loadJSONObject(this, "OPDS_prefs", "OPDSList");
+
+            final Iterator names = jsonObj.keys();
+            while (names.hasNext()) {
+                String key = (String) names.next();
+                feeds.put(key, jsonObj.getString(key));
+            }
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+        if (feeds.isEmpty()) {
+            feeds.put("Flibusta", "http://flibusta.net/opds");
+            feeds.put("Plough", "http://www.plough.com/ploughCatalog_opds.xml");
+        }
+
+        recreateAdapter();
+
+    }
+
+    protected void addFeed(String alias, String url) {
+        if (LengthUtils.isNotEmpty(alias) && LengthUtils.isNotEmpty(url)) {
+            feeds.put(alias, url);
+
+            JSONSharedPreferences.saveJSONObject(this, "OPDS_prefs", "OPDSList", new JSONObject(feeds));
+
+            recreateAdapter();
+        }
+    }
+
+    private void recreateAdapter() {
+        if (adapter != null) {
+            adapter.close();
+        }
+
+        ArrayList<Feed> tmpFeeds = new ArrayList<Feed>();
+        for (java.util.Map.Entry<String, String> entry : feeds.entrySet()) {
+            tmpFeeds.add(new Feed(entry.getKey(), entry.getValue()));
+        }
+
+        adapter = new OPDSAdapter(this, tmpFeeds.toArray(new Feed[tmpFeeds.size()]));
+        adapter.addListener(this);
+
+        list.setAdapter(adapter);
     }
 
     @Override
@@ -97,6 +188,7 @@ public class OPDSActivity extends AbstractActionActivity implements ExpandableLi
         updateNavigation(feed);
 
         setTitle(feed != null ? feed.title : "OPDS feeds");
+        findViewById(R.id.opdsaddfeed).setVisibility(feed != null ? View.GONE : View.VISIBLE);
         adapter.setCurrentFeed(feed);
     }
 
---
+++ branches/opds/src/org/emdev/utils/preferences/JSONSharedPreferences.java
@@ -0,0 +1,46 @@
+package org.emdev.utils.preferences;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+public class JSONSharedPreferences {
+    private static final String PREFIX = "json";
+
+    public static void saveJSONObject(Context c, String prefName, String key, JSONObject object) {
+        SharedPreferences settings = c.getSharedPreferences(prefName, 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString(JSONSharedPreferences.PREFIX+key, object.toString());
+        editor.commit();
+    }
+
+    public static void saveJSONArray(Context c, String prefName, String key, JSONArray array) {
+        SharedPreferences settings = c.getSharedPreferences(prefName, 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString(JSONSharedPreferences.PREFIX+key, array.toString());
+        editor.commit();
+    }
+
+    public static JSONObject loadJSONObject(Context c, String prefName, String key) throws JSONException {
+        SharedPreferences settings = c.getSharedPreferences(prefName, 0);
+        return new JSONObject(settings.getString(JSONSharedPreferences.PREFIX+key, "{}"));
+    }
+
+    public static JSONArray loadJSONArray(Context c, String prefName, String key) throws JSONException {
+        SharedPreferences settings = c.getSharedPreferences(prefName, 0);
+        return new JSONArray(settings.getString(JSONSharedPreferences.PREFIX+key, "[]"));
+    }
+
+    public static void remove(Context c, String prefName, String key) {
+        SharedPreferences settings = c.getSharedPreferences(prefName, 0);
+        if (settings.contains(JSONSharedPreferences.PREFIX+key)) {
+            SharedPreferences.Editor editor = settings.edit();
+            editor.remove(JSONSharedPreferences.PREFIX+key);
+            editor.commit();
+        }
+    }
+}
