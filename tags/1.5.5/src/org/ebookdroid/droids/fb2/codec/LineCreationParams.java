279042cc974a27b40df452cea1d1918d0ce45201
alexander.v.kasatkin <alexander.v.kasatkin@714301b5-5bf2-3aa6-4ccc-ecebab0412e3>
2012-10-17 16:41:08 +0000

    EBookDroid v1.5.5

---
+++ tags/1.5.5/src/org/ebookdroid/droids/fb2/codec/LineCreationParams.java
@@ -0,0 +1,12 @@
+package org.ebookdroid.droids.fb2.codec;
+
+import org.emdev.common.textmarkup.JustificationMode;
+
+public class LineCreationParams {
+
+    public JustificationMode jm;
+    public int maxLineWidth;
+    public boolean insertSpace = true;
+    public ParsedContent content;
+    public int extraSpace = 0;
+}
