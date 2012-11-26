77db2052d8d9ed277831e1363855c0d4cafadf20
alexander.v.kasatkin <alexander.v.kasatkin@714301b5-5bf2-3aa6-4ccc-ecebab0412e3>
2012-11-26 08:39:57 +0000

    Remove file notifications.

--- src/org/ebookdroid/common/notifications/OldestNotificationManager.java
+++ src/org/ebookdroid/common/notifications/OldestNotificationManager.java
@@ -1,9 +1,6 @@
 package org.ebookdroid.common.notifications;
 
-import org.ebookdroid.R;
-
 import android.annotation.TargetApi;
-import android.app.Notification;
 import android.content.Intent;
 
 @TargetApi(3)
@@ -11,12 +8,7 @@ class OldestNotificationManager extends AbstractNotificationManager {
 
     @Override
     public int notify(final CharSequence title, final CharSequence message, final Intent intent) {
-        final String text = title + ": " + message;
-
-        final Notification notification = new Notification(R.drawable.application_icon, text, System.currentTimeMillis());
         final int id = SEQ.getAndIncrement();
-
-        getManager().notify(id, notification);
         return id;
     }
 }
